805257c2737a466019c0447589bca0894ff078ed
Taras Polovyi <taras.polovyi@ua.ibm.com>
2018-06-12 17:54:20 +0300

    Two variants how to use fetch!

--- REACT/app/src/index.js
+++ REACT/app/src/index.js
@@ -1544,3 +1544,36 @@ render(
     <FilterableProductTable products={PRODUCTS}/>,
     document.getElementById('root')
 );
+
+
+
+//Fetch js
+
+// const getPosts = () =>{
+//     return fetch(`http://jsonApiPlaceholder.com/posts`)
+//     .then(res=>res.json())
+//     .then(posts => console.log(posts))
+// }
+//===========================================================>
+// const post={
+//     title:'All about fetch!',
+//     body:'Fetch is so cool!',
+//     userID:1
+// }
+
+// const newPost =post=>{
+//    const options={
+//        method:'POST',
+//        body:JSON.stringify(post),
+//        headers:new Headers({
+//            'Content-Type':'application/json'
+//        })
+//    }
+//     return fetch(`http://jsonApiPlaceholder.com/posts`,options)
+//     .then(res=>res.json())
+//     .then(res => console.log(res))
+//     .catch(error => console.error(`Error:${error}`))
+
+// }
+
+// newPost(post);
